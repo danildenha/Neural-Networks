5c09272a077ada03be5facf1ef3f00bcea3bd223
danildenha <danildenha@gmail.com>
2023-12-17 10:20:28 -0600

    Creating first perceptons

---
+++ Neural_1.1/Neural.cpp
@@ -0,0 +1,16 @@
+#include "Neural.h"
+
+double frand(){
+	return (2.0*(double)rand() / RAND_MAX) - 1.0;
+}
+
+
+// Return a new Perceptron object with the specified number of inputs (+1 for the bias).
+Perceptron::Perceptron(size_t inputs, double bias){
+
+}
+
+// Run the perceptron. x is a vector with the input values.
+double Perceptron::run(std::vector<double> x){
+	return 0.0;
+}
