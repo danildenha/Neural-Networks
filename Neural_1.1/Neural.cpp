599445e2e4ba049797286f67f5413188c9f43d90
danildenha <danildenha@gmail.com>
2023-12-17 10:24:28 -0600

    Update Neural.cpp

--- Neural_1.1/Neural.cpp
+++ Neural_1.1/Neural.cpp
@@ -5,9 +5,11 @@ double frand(){
 }
 
 
-// Return a new Perceptron object with the specified number of inputs (+1 for the bias).
+// Constructor for a New Perceptron object with the specified number of inputs +1 for the bias.
 Perceptron::Perceptron(size_t inputs, double bias){
-
+    this->bias = bias;
+    weights.resize(inputs + 1);
+    generate(weights.begin(), weights.end(), frand);
 }
 
 // Run the perceptron. x is a vector with the input values.
