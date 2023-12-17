9ff998e34e5ac00b682296e87de572a574c09f6d
danildenha <danildenha@gmail.com>
2023-12-17 10:46:26 -0600

    create main

---
+++ Neural_1.1/Driver.cpp
@@ -0,0 +1,8 @@
+// Driver.cpp : This file contains the 'main' function
+
+#include "Neural.h"
+
+int main(){
+    srand(time(NULL));
+    rand();
+}
--- Neural_1.1/Neural.cpp
+++ Neural_1.1/Neural.cpp
@@ -18,3 +18,12 @@ double Perceptron::run(std::vector<double> x){
     double sum = inner_product(x.begin(), x.end(), weights.begin(), (double)0.0);
     return sigmoid(sum);
 }
+
+void Perceptron::set_weights(std::vector<double> w_init) {
+    weights = w_init;
+}
+
+
+double Perceptron::sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
