6c970b64a27cf1cd75d901dff21512beacda16e6
danildenha <danildenha@gmail.com>
2023-12-17 11:57:59 -0600

    changes

--- Neural_Network_AND/Driver.cpp
+++ Neural_Network_AND/Driver.cpp
@@ -10,11 +10,11 @@ int main(){
     std::cout << "\n\n--------Logic Gate Example----------------\n\n";
     Perceptron p(2);
 
-    p.set_weights({10,10,-15}); //AND
+    p.set_weights({10.0,10.0,-15.0}); //AND
 
     std::cout << "Gate: " << std::endl;
-    std::cout << p.run({0,0}) << std::endl;
-    std::cout << p.run({0,1}) << std::endl;
-    std::cout << p.run({1,0}) << std::endl;
-    std::cout << p.run({1,1}) << std::endl;
+    std::cout << p.run({0.0,0.0}) << std::endl;
+    std::cout << p.run({0.0,1.0}) << std::endl;
+    std::cout << p.run({1.0,0.0}) << std::endl;
+    std::cout << p.run({1.0,1.0}) << std::endl;
 }
