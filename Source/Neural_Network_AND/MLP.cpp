b775ecd24cead6b9a8d01cb952d079ef2858fdbb
danildenha <danildenha@gmail.com>
2023-12-18 09:37:56 -0600

    Changes

--- Source/Neural_Network_AND/MLP.cpp
+++ Source/Neural_Network_AND/MLP.cpp
@@ -12,19 +12,19 @@ Perceptron::Perceptron(size_t inputs, double bias){
 	generate(weights.begin(),weights.end(),frand);
 }
 
-// Run the perceptron. x is a vector with the input values.
+// Runs the perceptron
 double Perceptron::run(std::vector<double> x){
 	x.push_back(bias);
 	double sum = inner_product(x.begin(),x.end(),weights.begin(),(double)0.0);
 	return sigmoid(sum);
 }
 
-// Set the weights. w_init is a vector with the weights.
+// Set the weights from the vector
 void Perceptron::set_weights(std::vector<double> w_init){
 	weights = w_init;
 }
 
-// Evaluate the sigmoid function for the floating point input x.
+// Evaluates the sigmoid function
 double Perceptron::sigmoid(double x){
 	return 1.0/(1.0 + exp(-x));
 }
